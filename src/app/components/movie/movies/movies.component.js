90b21d949fa06f5358d1ebccbf93d96157634764
Fagner Schwalm da Silva <fagnerschwalm@gmail.com>
2017-08-31 00:18:26 -0300

    feat: Add select to get movies orderBy criteria
    Need refactoring

--- src/app/components/movie/movies/movies.component.js
+++ src/app/components/movie/movies/movies.component.js
@@ -5,9 +5,42 @@
     .module('components.movie')
     .component('movies', {
       templateUrl: 'src/app/components/movie/movies/movies.html',
+      controller: Controller,
       bindings: {
         movies: '<'
       }
     });
+
+  Controller.$inject = ['MovieService'];
+
+
+  function Controller(MovieService) {
+    var vm = this;
+
+    vm.$onInit = onInit;
+    // TODO: Extrair para outro component e passar a função orderBy
+    // através de bindings.
+    vm.orderBy = orderBy;
+    vm.order = 'none';
+    vm.orderOptions = [
+      {
+        label: 'title',
+        value: 'title'
+      },
+      {
+        label: 'none',
+        value: ''
+      }
+    ]
+
+    function onInit() {
+    }
+
+    function orderBy() {
+      MovieService.getComicsOrderBy(vm.order.value).then(function(response){
+        vm.movies = response.data.data.results;
+      })
+    }
+  }
 
 }());
